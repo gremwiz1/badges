2ebbc3a134394bae66474444af4bb8db817b85db
gremwiz1 <gremwiz@yandex.ru>
2025-10-24 13:19:14 +0300

    delete shadow from svg and added in css

--- src/components/CountRedShadowBadge.vue
+++ src/components/CountRedShadowBadge.vue
@@ -1,23 +1,9 @@
 <template>
   <div class="count-red-shadow-badge">
     <svg class="count-red-shadow-badge__shape" viewBox="0 0 25 19" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
-    <g filter="url(#filter0_d_1_145)">
     <path d="M3.00067 19L5.48982 19L5.48982 -4.05292e-07L5.0533 -3.75805e-07C3.57708 -2.76089e-07 2.32025 1.07395 2.09001 2.53211L0.0373846 15.5321C-0.250123 17.353 1.15723 19 3.00067 19Z" fill="currentColor"/>
     <rect width="14" height="19" transform="translate(5.48982)" fill="currentColor"/>
     <path d="M21.979 2.33265e-07L19.4898 3.98647e-07L19.4898 19L19.9263 19C21.4026 19 22.6594 17.926 22.8896 16.4679L24.9423 3.46789C25.2298 1.647 23.8224 1.10784e-07 21.979 2.33265e-07Z" fill="currentColor"/>
-    </g>
-    <defs>
-    <filter id="filter0_d_1_145" x="-0.510178" y="0" width="26" height="19" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-    <feOffset dy="4"/>
-    <feGaussianBlur stdDeviation="2"/>
-    <feComposite in2="hardAlpha" operator="out"/>
-    <feColorMatrix type="matrix" values="0 0 0 0 0.117647 0 0 0 0 0.0627451 0 0 0 0 0 0 0 0 0.25 0"/>
-    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_145"/>
-    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_145" result="shape"/>
-    </filter>
-    </defs>
     </svg>
     
     <!-- Content поверх SVG -->
@@ -60,6 +46,7 @@ defineProps({
   width: 100%;
   height: 100%;
   color: #FB044A;
+  filter: drop-shadow(0 4px 4px rgba(30, 16, 0, 0.25));
 }
 
 .count-red-shadow-badge__content {
--- src/components/PinkBadge.vue
+++ src/components/PinkBadge.vue
@@ -1,23 +1,9 @@
 <template>
   <div class="pink-badge">
     <svg class="pink-badge__shape" viewBox="0 0 69 19" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
-    <g filter="url(#filter0_d_1_145)">
     <path d="M3.00067 19L5.48982 19L5.48982 -4.05292e-07L5.0533 -3.75805e-07C3.57708 -2.76089e-07 2.32025 1.07395 2.09001 2.53211L0.0373846 15.5321C-0.250123 17.353 1.15723 19 3.00067 19Z" fill="currentColor"/>
     <rect width="58" height="19" transform="translate(5.48982)" fill="currentColor"/>
     <path d="M65.979 2.33265e-07L63.4898 3.98647e-07L63.4898 19L63.9263 19C65.4026 19 66.6594 17.926 66.8896 16.4679L68.9423 3.46789C69.2298 1.647 67.8224 1.10784e-07 65.979 2.33265e-07Z" fill="currentColor"/>
-    </g>
-    <defs>
-    <filter id="filter0_d_1_145" x="-0.510178" y="0" width="70" height="19" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-    <feOffset dy="4"/>
-    <feGaussianBlur stdDeviation="2"/>
-    <feComposite in2="hardAlpha" operator="out"/>
-    <feColorMatrix type="matrix" values="0 0 0 0 0.117647 0 0 0 0 0 0 0 0 0 0.0901961 0 0 0 0.25 0"/>
-    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_145"/>
-    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_145" result="shape"/>
-    </filter>
-    </defs>
     </svg>
     
     <!-- Content поверх SVG -->
@@ -72,6 +58,7 @@ defineProps({
   width: 100%;
   height: 100%;
   color: #FF2EAB;
+  filter: drop-shadow(0 4px 4px rgba(30, 0, 23, 0.25));
 }
 
 .pink-badge__content {
